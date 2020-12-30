a9ba20fd8ad58e6b51d9ecd95b9967ccaa8d988c
Joao-0902 <71885261+Joao-0902@users.noreply.github.com>
2020-12-30 15:33:25 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,71 @@
+function soma(){
+  let tn1 = document.querySelector('#tn1')
+  let tn2 = document.querySelector('#tn2')
+  p = document.querySelector('#res')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  let res = n1 + n2
+  p.innerHTML = res
+}
+function sub(){
+  let tn1 = document.querySelector('#tn1')
+  let tn2 = document.querySelector('#tn2')
+  p = document.querySelector('#res')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  let res = n1 - n2
+  p.innerHTML = res
+}
+function mult(){
+  let tn1 = document.querySelector('#tn1')
+  let tn2 = document.querySelector('#tn2')
+  p = document.querySelector('#res')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  res = n1 * n2
+  p.innerHTML = res
+}
+function div(){
+  let tn1 = document.querySelector('#tn1')
+  let tn2 = document.querySelector('#tn2')
+  p = document.querySelector('#res')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  res = n1 / n2
+  p.innerHTML = res
+}function soma(){
+  let tn1 = document.querySelector('#tn1')
+  let tn2 = document.querySelector('#tn2')
+  p = document.querySelector('#res')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  let res = n1 + n2
+  p.innerHTML = res
+}
+function sub(){
+  let tn1 = document.querySelector('#tn1')
+  let tn2 = document.querySelector('#tn2')
+  p = document.querySelector('#res')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  let res = n1 - n2
+  p.innerHTML = res
+}
+function mult(){
+  let tn1 = document.querySelector('#tn1')
+  let tn2 = document.querySelector('#tn2')
+  p = document.querySelector('#res')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  res = n1 * n2
+  p.innerHTML = res
+}
+function div(){
+  let tn1 = document.querySelector('#tn1')
+  let tn2 = document.querySelector('#tn2')
+  p = document.querySelector('#res')
+  let n1 = Number(tn1.value)
+  let n2 = Number(tn2.value)
+  res = n1 / n2
+  p.innerHTML = res
+}
